1824a388f76ef32b9a0a302522af1f69e3bd85a2
anandkprajapati123 <anandkprajapati123@gmail.com>
2025-04-01 13:42:33 +0530

    Add files via upload

---
+++ COMPLETE JS/Lecture6/DOM.js
@@ -0,0 +1,33 @@
+// console.dir(document.body.childNodes[1]);
+// console.dir(document.head);
+// window.console.log("Hello");
+// console.log(document.body.style.background="green")
+// document.body.childNodes[1].innerText="abcd";  
+
+// let heading=document.getElementById("heading");
+// console.log(heading);
+// console.dir(heading);
+
+// let paras=document.getElementsByClassName("paragraph");
+// console.log(paras);
+// console.dir(paras);
+
+// let para=document.getElementsByTagName("p")
+// console.log(para)
+// console.dir(para)
+
+// let ele=document.querySelector("p");
+// let Allele=document.querySelector("p");
+// let elements=document.querySelector(".paragraph");
+// let element=document.querySelector("#btn")
+// console.log(ele);
+// console.log(Allele);
+// console.log(elements);
+// console.log(element);
+
+// console.dir(document.body.firstChild);
+// let div=document.querySelector("div");
+// console.dir(div);
+
+// let heading=document.querySelector("h1");
+// console.dir(heading);
