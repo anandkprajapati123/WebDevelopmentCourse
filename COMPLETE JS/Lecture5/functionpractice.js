00d56f139bc383669800e3c018c569afde7a018e
anandkprajapati123 <anandkprajapati123@gmail.com>
2025-04-10 10:13:54 +0530

    Update functionpractice.js

--- COMPLETE JS/Lecture5/functionpractice.js
+++ COMPLETE JS/Lecture5/functionpractice.js
@@ -1,16 +1,16 @@
 // Q.1
-// function countVowel(str){
-//     let count=0;
-//     for(const char of str){
-//         console.log(char);
-//         if(char==="a" ||    char==="e" || char==="i" || char==="o" || 
-//         char==="u"){
-//             count++;
-//         }
-//     }
-//     console.log(count);
-//     return count;
-// }
+function countVowel(str){
+    let count=0;
+    for(const char of str){
+        console.log(char);
+        if(char==="a" ||    char==="e" || char==="i" || char==="o" || 
+        char==="u"){
+            count++;
+        }
+    }
+    console.log(count);
+    return count;
+}
 
 // Q.2
 const countVowel =(str)=>{
