0b9450966b582005f19cabe4b9f05bc88d1a38aa
anandkprajapati123 <anandkprajapati123@gmail.com>
2025-04-10 10:13:23 +0530

    Update Function.js

--- COMPLETE JS/Lecture5/Function.js
+++ COMPLETE JS/Lecture5/Function.js
@@ -1,37 +1,37 @@
-// function myFunction(msg,n){ //function definition(parameter)
-//     console.log("Anand");
-//     console.log(msg*n); //output --> Not a Number
-// }
-// myFunction("i love JS",10); //function call(argument)
+function myFunction(msg,n){ //function definition(parameter)
+    console.log("Anand");
+    console.log(msg*n); //output --> Not a Number
+}
+myFunction("i love JS",10); //function call(argument)
 
-// function sum(a,b){
-    // local variables --> scope
-    // s=a+b;
-    // console.log(a);
-    // console.log("before return");
-    // return s;
-    // console.log("after return"); // not print
-// }
-// let val=sum(3,4);
-// console.log(val);
+function sum(a,b){
+    local variables --> scope
+    s=a+b;
+    console.log(a);
+    console.log("before return");
+    return s;
+    console.log("after return"); // not print
+}
+let val=sum(3,4);
+console.log(val);
 
-// Sum Function
-// function sum(a,b){
-//     return a+b;
-// }
+Sum Function
+function sum(a,b){
+    return a+b;
+}
 
-// mul function
-// function mul(c,d){
-//     return a*b;
-// }
+mul function
+function mul(c,d){
+    return a*b;
+}
 
-// // Use arrow function
-// const arrowmul=(a,b)=>{
-//     console.log(a*b);
-// }
+// Use arrow function
+const arrowmul=(a,b)=>{
+    console.log(a*b);
+}
 
-// arrowmul=6;
+arrowmul=6;
 
-// const printHello=()=>{
-//     console.log("hello");
-// };
+const printHello=()=>{
+    console.log("hello");
+};
