7d2c1fe215fcd7e5aa5782ec3525084ef05b3dcc
anandkprajapati123 <anandkprajapati123@gmail.com>
2025-04-10 10:12:56 +0530

    Update forEach_loop.js

--- COMPLETE JS/Lecture5/forEach_loop.js
+++ COMPLETE JS/Lecture5/forEach_loop.js
@@ -1,8 +1,8 @@
-// let arr=[1,2,3,4,5];
-// let cities=["Banaras","Delhi","Mumbai"]
-// arr.forEach(function printvalue(val){ //each val at each index
-//     console.log(val);
-// });
-// cities.forEach((val)=>{
-//     console.log(val.toUpperCase());
-// });
+let arr=[1,2,3,4,5];
+let cities=["Banaras","Delhi","Mumbai"]
+arr.forEach(function printvalue(val){ //each val at each index
+    console.log(val);
+});
+cities.forEach((val)=>{
+    console.log(val.toUpperCase());
+});
